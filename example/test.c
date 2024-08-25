f0277bca33dfe2abdfc4672db2fee30cb5d09002
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-25 20:04:45 +0800

    实现 "--" 前缀的 flag 检测
    还有很多很多 TODO 和需要优化的东西。
    但这是个基本思路

--- example/test.c
+++ example/test.c
@@ -1,12 +1,25 @@
+/*
+    Test it with these arguments:
+    ["hello", "--test", "testStr", "world"]
+ */
+
 #include <stdio.h>
 
 #include "parser.h"
 
 int main(int argc, char *argv[])
 {
-    struct parser parser[] = {};
+    char *test_str;
+    struct parser parser[] = {
+        {
+            .long_name = "test",
+            .var_type = kTypeString,
+            .variable_ptr = &test_str,
+        },
+    };
 
-    struct parser_result *res = ArgParser(argc, 0, argv, parser, 0);
+    struct parser_result *res = ArgParser(argc, 0, argv, parser, 1);
+    printf("test_str: %s\n", test_str);
     for (int i = 0; i < res->params_count; i++) {
         printf("%s\n", res->parameters[i]);
     }
--- parser.c
+++ parser.c
@@ -4,16 +4,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+static void GetNextArgumentWithType_(char argument[], void **var_ptr, enum parser_var_type var_type)
+{
+    switch (var_type) {
+    case kTypeString:
+        *var_ptr = argument;
+        break;
+    }
+
+    return;
+}
+
 struct parser_result *ArgParser(int argc, int last_arg, char *argv[], struct parser *options, int opt_num)
 {
     struct parser_result *res = malloc(sizeof(struct parser_result)); // res -> result
 
     for (; last_arg < argc; last_arg++) {
+        // TODO add "/", "+" prefix
         if (strncmp(argv[last_arg], "--", 2) == 0) {
-            // TODO
+            // TODO code optimization
+            // TODO implement "="
+            for (int i = 0; i < opt_num; i++) {
+                if (strcmp(options[i].long_name, argv[last_arg] + 2) != 0) // TODO make it be configurable
+                    continue;
+                last_arg++;
+                if (last_arg < argc)
+                    GetNextArgumentWithType_(argv[last_arg], options[i].variable_ptr, options[i].var_type);
+            }
             continue;
         } else if (strncmp(argv[last_arg], "-", 1) == 0) {
-            // TODO
+            // TODO same as "--"
             continue;
         } else {
             res->params_count += 1;
@@ -26,5 +46,6 @@ struct parser_result *ArgParser(int argc, int last_arg, char *argv[], struct par
     // last_arg equal with argc, but here need a valid index
     res->parsed_argc_index = last_arg - 1;
 
+    // TODO find a way to free those malloc()
     return res;
 }
