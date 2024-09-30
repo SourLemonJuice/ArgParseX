dd937a8310c4692c2a06e0a73cbf6dcd16d458de
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-09-30 21:32:56 +0800

    发布 v0.3

--- argpx.h
+++ argpx.h
@@ -7,9 +7,9 @@
 
 // the interface versions
 #define ARGPX_VERSION_MAJOR 0
-#define ARGPX_VERSION_MINOR 2
+#define ARGPX_VERSION_MINOR 3
 // a continuously growing integer, +1 for each release
-#define ARGPX_VERSION_REVISION 2
+#define ARGPX_VERSION_REVISION 3
 
 // details are in ArgpxStatusToString()
 enum ArgpxStatus {
